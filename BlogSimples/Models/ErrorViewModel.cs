2c328873132e6191ede83db8a670cd1d20e1a159
FelipeSilvaDev <felipeparra1423@gmail.com>
2025-07-24 14:30:42 -0300

    Projeto criado

---
+++ BlogSimples/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogSimples.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
